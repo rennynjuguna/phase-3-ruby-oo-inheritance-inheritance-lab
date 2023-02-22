dbd4c698684a337201da547b3e670489da3b0648
Renny Muragu <118157651+rennynjuguna@users.noreply.github.com>
2023-02-22 10:02:12 +0300

    Completed Assignment

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+    def first_name=(firstname)
+        @first_name = firstname
+    end
+    def first_name
+        @first_name
+    end
+    def last_name=(lastname)
+        @last_name = lastname
+    end
+    def last_name
+        @last_name
+    end
+end
