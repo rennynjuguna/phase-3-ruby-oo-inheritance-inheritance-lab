525c6ad3793018c1d022b9ff54a5e0cc993a3480
Renny Muragu <118157651+rennynjuguna@users.noreply.github.com>
2023-02-22 10:00:57 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user'
 
+class Student < User
+ def initialize 
+    @knowledge = []
+ end
+ def learn vale
+    @knowledge << vale
+
+ end
+ def knowledge
+    @knowledge
+ end
 end
